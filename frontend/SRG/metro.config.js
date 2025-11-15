7e2c8c6490225e1656a0c1e715e5a16fd1300ca1
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-11-15 08:32:31 +0200

    Fix metro.config.js to use @expo/metro-config

--- frontend/SRG/metro.config.js
+++ frontend/SRG/metro.config.js
@@ -1,8 +1,11 @@
 // Learn more https://docs.expo.dev/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Add any custom metro config here if needed
+// config.resolver.assetExts.push('custom-ext');
 
 module.exports = config;
 
